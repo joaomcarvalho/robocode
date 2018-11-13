a7981dbcabb38240e03bce916754946e3dcc9660
João Maurício <joao.carvalho@ccc.ufcg.edu.br>
2018-11-13 06:50:31 -0300

    Done

--- robots/jeuj/JeujDaPeu.java
+++ robots/jeuj/JeujDaPeu.java
@@ -10,31 +10,31 @@ public void run() {
     setColors(Color.red,Color.blue,Color.green);
 
     while (true) {
-        turnGunRight(15.813076205841048);
-        setAhead(461.4493284332109);
+        turnGunRight(20.30561967752975);
+        setAhead(339.8786182192478);
     }
 }
 public void onScannedRobot(ScannedRobotEvent e) {
-    setAhead(23.238395127222145);
+    setAhead(46.206349269139245);
 
-    setTurnRight(150.42805895273304);
+    setTurnRight(62.02769513139049);
 
-    setTurnGunRight(211.7141966024935);
+    setTurnGunRight(370.38290095720924);
 
-    setTurnRadarRight(93.7939646998764);
+    setTurnRadarRight(0.05010490467607376);
 
-    if (e.getDistance() < 54.6570899011529) {
-        setFire(11.564541154334822);
+    if (e.getDistance() < 60.123847177154005) {
+        setFire(13.353009945606784);
     } else {
-        setFire(5.077038296882043);
+        setFire(7.887119210483245);
     }
 }
 public void onHitByBullet(HitByBulletEvent e) {
-    setTurnRight(125.4124599687636);
-    setAhead(474.41472694431445 * -1);
+    setTurnRight(401.09876017910517);
+    setAhead(229.75500696032302 * -1);
 }
 public void onHitWall(HitWallEvent e) {
-    back(100.77856249135313);
-    setAhead(239.1342452291164 * -1);
+    back(131.39765066471827);
+    setAhead(442.9924210980955 * -1);
 }
 }
